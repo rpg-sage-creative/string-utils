f9cbf12fd7f26fc3051b987baae550f5627e6132
Randal T Meyer <randal.t.meyer@gmail.com>
2024-02-20 14:04:36 -0800

    use allowDotNotation option

--- build/keyValueArg/getKeyValueArgSource.js
+++ build/keyValueArg/getKeyValueArgSource.js
@@ -2,7 +2,7 @@ import { getQuotedRegexSource } from "../quote/getQuotedRegexSource.js";
 import { getWordCharacterRegexSource } from "../regex/getWordCharacterRegexSource.js";
 import { getWhitespaceRegexSource } from "../whitespace/getWhitespaceRegexSource.js";
 export function getKeyValueArgSource(key) {
-    key = key ?? getWordCharacterRegexSource({ quantifier: "+" });
+    key = key ?? getWordCharacterRegexSource({ allowDotNotation: true, quantifier: "+" });
     const space = getWhitespaceRegexSource({ horizontalOnly: true, quantifier: "*" });
     const quotedRegexSource = getQuotedRegexSource({ lengthQuantifier: "*" });
     return `${key}${space}=${space}(?:${quotedRegexSource}|\\S+)`;
