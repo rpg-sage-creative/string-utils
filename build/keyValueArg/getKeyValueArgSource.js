f68d2c7a4fe84a6e49094e965c626a20bd0d3092
Randal T Meyer <randal.t.meyer@gmail.com>
2024-02-23 11:51:41 -0800

    strict key value args (no space around =)

--- build/keyValueArg/getKeyValueArgSource.js
+++ build/keyValueArg/getKeyValueArgSource.js
@@ -1,9 +1,7 @@
 import { getQuotedRegexSource } from "../quote/getQuotedRegexSource.js";
 import { getWordCharacterRegexSource } from "../regex/getWordCharacterRegexSource.js";
-import { getWhitespaceRegexSource } from "../whitespace/getWhitespaceRegexSource.js";
 export function getKeyValueArgSource(key) {
     key = key ?? getWordCharacterRegexSource({ allowDotNotation: true, quantifier: "+" });
-    const space = getWhitespaceRegexSource({ horizontalOnly: true, quantifier: "*" });
     const quotedRegexSource = getQuotedRegexSource({ lengthQuantifier: "*" });
-    return `${key}${space}=${space}(?:${quotedRegexSource}|\\S+)`;
+    return `${key}=(?:${quotedRegexSource}|\\S+)`;
 }
