5b9dc9378abf45b9fb1de6a3ef5c3e072b470e8e
Randal T Meyer <randal.t.meyer@gmail.com>
2024-02-27 14:39:43 -0800

    include emptystring test for chunk

--- test/chunk/chunk.mjs
+++ test/chunk/chunk.mjs
@@ -2,12 +2,13 @@ import { debug, info, warn } from "@rsc-utils/console-utils";
 import { assert, runTests, startAsserting, stopAsserting } from "@rsc-utils/test-utils";
 import { chunk } from "../../build/index.js";
 
-runTests(async function testChunk() {
+runTests(async function test_chunk() {
 	// [expectedArray, inputString, maxLength]
 	const input = [
 		[["this is\n a sentence\n that has a\n bunch of words."], "this is\n a sentence\n that has a\n bunch of words.", 2000],
 		[["this is", " a sentence", " that has a", " bunch of", " words."], "this is\n a sentence\n that has a\n bunch of words.", 15],
 		[["this is a", " sentence that", " has a bunch", " of words."], "this is a sentence that has a bunch of words.", 15],
+		[[], " \t \n ".trim(), 2000],
 	];
 	input.forEach(([expected, value, length]) => {
 		assert(expected, chunk, value, length);
